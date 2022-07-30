d7d56424b822a05a8b7ca1899d1f4eb6eb503149
skandog <muumrar@gmail.com>
2022-07-30 14:51:45 +0100

    normalised the line endings

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3731e1074de71f1d"; }
+/******/ 		__webpack_require__.h = function() { return "bed1843ff11369af"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import NextLink from 'next/link'
 import {
   Container,
   Box,
   Heading,
+  Link,
   useColorModeValue,
-  Image
+  Image,
+  Button
 } from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
 import Section from '../components/section'
-import Paragraph from '../components/paragraph'
+// import Paragraph from '../components/paragraph'
 
 const Page = () => {
   const dogImg = `/images/skandy-logo${useColorModeValue('', '-dark')}-v3.png`
@@ -55,14 +59,21 @@ const Page = () => {
         <Heading as="h3" variant="section-title">
           Work
         </Heading>
-        <Paragraph>
+        <p style={{ 'text-align': 'justify', 'text-indent': '1em' }}>
           Skandy is a software developer with a love for growth and embracing
           new challenges. Born and raised in London, he lives with his partner,
           pizzas and plants 🌱. When not at the keyboard he enjoys trying new
           recipes and sharing them with his friends and family. Currently he is
           expanding his abilty to build beautiful full-stack applications with
-          The School of Code, while working freelance and open to full time roles.
-        </Paragraph>
+          School of Code, while working freelance and open to full time roles.
+        </p>
+        <Box align="left" my={4}>
+          <NextLink href="/projects">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="green">
+              Projects
+            </Button>
+          </NextLink>
+        </Box>
       </Section>
     </Container>
   )
