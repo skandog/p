af259b660010d99719fb7c73bd01df4733b25356
skandog <muumrar@gmail.com>
2022-07-30 16:06:40 +0100

    reverted next.config to commonjs as was causing compile errors fatal

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4c8caa15ecd93d8d"; }
+/******/ 		__webpack_require__.h = function() { return "942d3ae1b68f4b73"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
