2afdc4d0eb7c67a7d917f8c68a35dcdfb69f2876
skandog <muumrar@gmail.com>
2022-07-30 18:01:33 +0100

    added grid item components in bid to start making cards, but having some difficulty rendering the new component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9b0145e467df9064"; }
+/******/ 		__webpack_require__.h = function() { return "6d730bebba373c98"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/projects.js
+++ pages/projects.js
@@ -1,13 +1,34 @@
-import { Box, Container } from "@chakra-ui/react";
-
+import { Box, Container, SimpleGrid, Heading, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+import skandy from '../public/images/skandy-logo-v3.png'
 
 const Projects = () => {
-    return ( 
-        <Container>
-
-        <Box borderRadius="lg" bg="blue.700" color="whiteAlpha.800" p={3} mt={10} mb={6} align="center" >Here are some of my previous work</Box>
-        </Container>
-     );
+  return (
+    <Container>
+      <Box
+        borderRadius="lg"
+        bg="blue.700"
+        color="whiteAlpha.800"
+        p={3}
+        mt={10}
+        mb={6}
+        align="center"
+      >
+        Here is some of my favourite work
+      </Box>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Projects
+      </Heading>
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>
+          <WorkGridItem id="skandy" title="Skandy" thumbnail={skandy}>
+            A skandy app
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
+    </Container>
+  )
 }
- 
-export default Projects;
+
+export default Projects
