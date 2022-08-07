821e712b05edb7b756d41de4ddf4e26321f5b730
skandog <muumrar@gmail.com>
2022-08-07 10:32:05 +0100

    created custom 404 page and tested it works for any erroneous path

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ecbe25ea3ef5ff72"; }
+/******/ 		__webpack_require__.h = function() { return "387ab94bb0c021a3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.97a31a7fa031ee76.hot-update.js
+++ .next/static/webpack/webpack.97a31a7fa031ee76.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ddba37df99f27511"; }
+/******/ 	__webpack_require__.h = function() { return "0b82627935705b24"; }
 /******/ }();
 /******/ 
 /******/ }
