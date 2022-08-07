dbc790489d5eb88595078b9a82127abd41f40561
skandog <muumrar@gmail.com>
2022-08-07 13:49:24 +0100

    inital commit

---
+++ pages/thanks.js
@@ -0,0 +1,24 @@
+import { Box, Container } from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
+
+const Thanks = () => {
+  return (
+    <Layout>
+      <Container>
+        <Box
+          borderRadius="lg"
+          bg="red.700"
+          color="whiteAlpha.800"
+          p={3}
+          mt={10}
+          mb={6}
+          align="center"
+        >
+          Thanks
+        </Box>
+      </Container>
+    </Layout>
+  )
+}
+
+export default Posts
