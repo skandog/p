5f5026af0fd20507978ad07714489cf03c945936
skandog <muumrar@gmail.com>
2022-08-13 15:13:06 +0100

    really just adding copyy to the backpack page

--- pages/projects/backpack.js
+++ pages/projects/backpack.js
@@ -1,11 +1,6 @@
-import {
-  Container,
-  Badge,
-  Link,
-  List,
-  ListItem,
-  useColorModeValue
-} from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem, Divider } from '@chakra-ui/react'
+import NextLink from 'next/link'
+
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, ProjectImage, Meta } from '../../components/project.js'
 import P from '../../components/paragraph.js'
@@ -20,8 +15,36 @@ const Project = () => {
           Backpack - A study aid for bootcampers <Badge ml={'10px'}>2022</Badge>
         </Title>
         <P>
-          This was a project that we undertook 
+          This was a project in which I worked with{' '}
+          <NextLink href="https://github.com/AdamPhasey" passHref>
+            <Link target="_blank">Adam</Link>
+          </NextLink>
+          ,{' '}
+          <NextLink href="https://github.com/ClaudiaGC1339" passHref>
+            <Link target="_blank">Claudia</Link>
+          </NextLink>{' '}
+          and{' '}
+          <NextLink href="https://github.com/StevenCK1" passHref>
+            <Link target="_blank">Steven</Link>
+          </NextLink>{' '}
+          to build a full stack app in just one week. We had not worked together
+          before that week, and were given the simple breif to &apos;build an
+          app to help fellow students&apos;.
           <br />
+          <br />
+          We started by getting to know eachother, sharing our strengths and
+          weaknesses. We agreed how we wanted to work as a team before we even
+          started tackling the problem.
+          <br />
+          <br />
+          We worked well through the week. And while the enormity of the task
+          didnt dawn on us until we were halfway through, we were pleased with
+          reaching our mvp v.1.0.
+          <br />
+          <br />
+          The general premise of the app is to help students to keep track of
+          their development, by breaking down the topics into individual skills,
+          as well as tools to assist in improving those skills.
         </P>
         <List ml={4} my={2}>
           <ListItem>
