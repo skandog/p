71f92f1cfb2284d8181ef85a5c60f1eba377e974
skandog <muumrar@gmail.com>
2022-11-25 10:40:45 +0000

    installed google tag manager scripts to the main layout

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import { Box, Container } from '@chakra-ui/react'
 import Navbar from '../Navbar'
+import Script from 'next/script'
 
 const Main = ({ children, router }) => {
   return (
@@ -14,6 +15,19 @@ const Main = ({ children, router }) => {
         />
         <title>Skanfolio</title>
       </Head>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KXQ86PK');`}
+      </Script>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KXQ86PK"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }}
+      />
       <Navbar path={router.asPath} />
       <Container maxW="container.md" pt={6}>
         {children}
