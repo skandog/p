bc39bc3473b4e40aae95c21ba79a52aab8263cad
skandog <muumrar@gmail.com>
2022-12-16 12:10:55 +0000

    4 hour weather updated

--- pages/projects/four-hour-weather.js
+++ pages/projects/four-hour-weather.js
@@ -22,12 +22,13 @@ const Project = () => {
         </Title>
         <P>
           This project was a front-end exercise that a friend set me. The
-          primary challenge was to deliver in a short amount of time, just four
-          hours. The secondary challenge was to build the application using Scss
-          while not relying on in-built grid systems such as bootstrap.
+          primary challenge was to deliver in a short amount of time, just three
+          hours. The secondary challenge was to build the application using
+          SCSS/SASS while not relying on in-built grid systems such as
+          bootstrap.
           <br />
-          <br />I enjoyed this build as it really stretched me; I have found
-          positioning using only CSS difficult since the first day I started
+          <br />I enjoyed this build as it stretched me further than I expected;
+          I have found positioning using only CSS challenging since I started
           coding, and the time pressure really helped to motivate me and develop
           a better grasp of the fundamentals.
           <br />
@@ -35,6 +36,12 @@ const Project = () => {
           Given that time was the primary challenge, I had to stop even though I
           did not feel that the project was complete. While I had hit all of the
           targets set, I would have loved to add a lot more attractive design.
+          <br />
+          <br />I deliberated whether to share this, as it is not at the level
+          that I would be happy shipping for a client, but ultimately decided it
+          is worth showing my development. I regularly appraise my skill set for
+          areas that could benefit from more learning and work on those to
+          ensure I remain a well rounded full-stack developer.
         </P>
         <List ml={4} my={2}>
           <ListItem>
@@ -57,7 +64,7 @@ const Project = () => {
           </ListItem>
           <ListItem>
             <Meta>Stack</Meta>
-            <span>JavaScript, Next.js, React, Scss, OpenWeatherAPI</span>
+            <span>JavaScript, Next.js, React, SCSS, OpenWeatherAPI</span>
           </ListItem>
         </List>
         <Divider my={8} />
