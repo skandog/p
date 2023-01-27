22dc2b728d1def653e6f565ae48d16e605f8373a
skandog <muumrar@gmail.com>
2023-01-27 17:30:38 +0000

    adjusted some of the copy

--- pages/projects/pomfocus.js
+++ pages/projects/pomfocus.js
@@ -35,16 +35,20 @@ const Project = () => {
           </NextLink>
           , using this in my daily flow to optimise my focus, I chose to build a
           Pomodoro timer app. <br /> <br />
-          The most challenging aspect of mobile development, I have found, is
-          managing the environments. As such I used Expo to build the app, which
-          made it easy to get up and running quickly. Unfortunately the app is
-          not yet available for download, but I plan to continue learning and
-          release it in the future.
+          The most challenging aspect of mobile development tends to be managing
+          the various environments. As such I used Expo to build the app, which
+          made it easy to get up and running quickly on both Android and iOS.
+          WHile the app is not yet available for download, I plan to continue
+          learning and release it sometime in the future.
           <br /> <br />
-          This project was a lot of fun to build as I am a big sci-fi fan and
-          decided to style it with a space background and neon theme. I also
-          created a custom hook to check whether the keyboard is open so that I
-          can change the number of items shown in the focus history.
+          This project was a lot of fun as I am a big sci-fi fan and decided to
+          style it with a space background and neon theme. I also created a
+          custom hook to check whether the keyboard is open so that I can change
+          the number of items shown in the focus history.
+          <br />
+          <br />
+          I&apos;m definitely hooked on mobile developemnt and look forward to
+          bringing you more in the near future.
           <br />
           <br />
         </P>
